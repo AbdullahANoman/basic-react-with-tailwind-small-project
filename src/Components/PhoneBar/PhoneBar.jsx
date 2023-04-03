eeb41c3786905dad0a8310f85afdc349930a2a90
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-04-03 20:42:18 +0600

    add axios how to use

--- src/Components/PhoneBar/PhoneBar.jsx
+++ src/Components/PhoneBar/PhoneBar.jsx
@@ -1,15 +1,48 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import axios from "axios";
 const PhoneBar = () => {
-    const [phones,setPhones] = useState([]);
+  const [phones, setPhones] = useState([]);
 
-    axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-    .then(data=>console.log(data))
-    return (
-        <div>
-            
-        </div>
-    );
+  axios
+    .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+    .then((data) => {
+      const loadedData = data.data.data;
+      const phoneData = loadedData.map((phone) => {
+        const resultData = phone.slug.split("-");
+        const intResultData = parseInt(resultData[1]);
+        const price = intResultData;
+        const phoneInfo = {
+          name: phone.phone_name,
+          price: price,
+        };
+        return phoneInfo;
+      });
+      // setPhones(phoneData)
+      // console.log(phoneData)
+      setPhones(phoneData);
+    });
+  return (
+    <div>
+      <ResponsiveContainer width={700} height={700}>
+        <BarChart width={1600} height={400} data={phones}>
+          <Bar dataKey="price" fill="#8884d8" />
+          <XAxis dataKey="name"></XAxis>
+          <YAxis dataKey="price"></YAxis>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default PhoneBar;
